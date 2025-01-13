4648c903d510b0d7ff7142b461b3e0c5d95c99ec
BAH Mamad <mbah@student.42lyon.fr>
2025-01-13 21:32:04 +0100

    feat(main): add basic MiniLibX initialization and pixel drawing
    - Added my_mlx_pixel_put function to draw a pixel on the screen
    - Initialized MiniLibX and created a window with basic setup in main
    - Displayed a single red pixel at coordinates (5, 5)

--- src/main.c
+++ src/main.c
@@ -6,16 +6,34 @@
 /*   By: mbah <mbah@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/09 15:26:16 by mbah              #+#    #+#             */
-/*   Updated: 2025/01/09 15:34:22 by mbah             ###   ########.fr       */
+/*   Updated: 2025/01/09 22:49:48 by mbah             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../inc/FdF.h"
+#include "FdF.h"
+
+void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
+{
+	char	*dst;
+
+	dst = data->addr + (y * data->line_length + x * (data->bits_per_pixel / 8));
+	*(unsigned int *) dst = color;
+}
 
 int	main(int argc, char **argv)
 {
-	(void) argc;
-	(void) argv;
+	void	*mlx;
+	void	*mlx_win;
+	t_data	img;
 
-	return (0);
+	if (argc < 2)
+		return (0);
+	mlx = mlx_init();
+	mlx_win = mlx_new_window(mlx, 520, 480, ft_strdup("Hello world!"));
+	img.img = mlx_new_image(mlx, 520, 480);
+	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length,
+			&img.endian);
+	my_mlx_pixel_put(&img, 5, 5, 0x00FF0000);
+	mlx_put_image_to_window(mlx, mlx_win, img.img, 0, 0);
+	mlx_loop(mlx);
 }
