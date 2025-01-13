fb0419d795a5da2e746a6657c8391804915e576e
BAH Mamad <mbah@student.42lyon.fr>
2025-01-13 21:30:31 +0100

    feat(headers): add new headers and update FdF.h
    - Added new header files:
      - ft_printf.h
      - get_next_line.h
      - libft.h
    - Modified FdF.h to include necessary declarations and updates.

--- inc/FdF.h
+++ inc/FdF.h
@@ -6,13 +6,27 @@
 /*   By: mbah <mbah@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/09 15:25:47 by mbah              #+#    #+#             */
-/*   Updated: 2025/01/09 15:34:26 by mbah             ###   ########.fr       */
+/*   Updated: 2025/01/09 22:48:18 by mbah             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FDF_H
 # define FDF_H
 
+# include <mlx.h>
 # include <unistd.h>
+# include <stdlib.h>
+# include <math.h>
+# include "ft_printf.h"
+# include "libft.h"
+
+typedef struct s_data
+{
+	void	*img;
+	char	*addr;
+	int		bits_per_pixel;
+	int		line_length;
+	int		endian;
+}			t_data;
 
 #endif
