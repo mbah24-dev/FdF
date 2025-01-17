5efa4318c4e31b5e33a04e201c239297306eaeb6
BAH Mamad <mbah@student.42lyon.fr>
2025-01-17 22:29:39 +0100

    refactor(FdF): update FdF.h

--- inc/FdF.h
+++ inc/FdF.h
@@ -6,7 +6,7 @@
 /*   By: mbah <mbah@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/09 15:25:47 by mbah              #+#    #+#             */
-/*   Updated: 2025/01/16 17:35:20 by mbah             ###   ########.fr       */
+/*   Updated: 2025/01/17 17:56:00 by mbah             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,5 +20,26 @@
 # include "ft_printf.h"
 # include "libft.h"
 # include "FdF_utils.h"
+# include "FdF_key.h"
+# include <stdio.h>
+# include <time.h>
+
+typedef struct s_vars
+{
+	void		*mlx;
+	void		*mlx_win;
+	t_data		image;
+	t_point		circle_pos;
+	int			keycode;
+	t_sprite	moquette;
+}				t_vars;
+
+typedef enum e_flag
+{
+	OK = 1,
+	ERROR = 0
+}	t_flag;
+
+int	close_win(t_vars *vars);
 
 #endif
