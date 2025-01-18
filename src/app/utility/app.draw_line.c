1bd9a9a670f6433db5d5f6042cc7848482bb9989
BAH Mamad <mbah@student.42lyon.fr>
2025-01-18 01:05:37 +0100

    feat: Implémentation de l'algorithme de Bresenham avec nouvelles structures
    - Ajout de la structure t_bresenham pour regrouper les variables nécessaires à l'algorithme de tracé de ligne de Bresenham.
    - Ajout de la structure t_line pour encapsuler les propriétés d'une ligne (pt_src, pt_dst, height, color).
    - Implémentation de la fonction draw_line_bresenham utilisant t_bresenham et t_line pour tracer des lignes efficacement.
    - Mise à jour de main.c pour utiliser la nouvelle approche de tracé de ligne.
    
    Ces changements améliorent la gestion du tracé des lignes et rendent le code plus modulaire et lisible.

--- src/app/utility/app.draw_line.c
+++ src/app/utility/app.draw_line.c
@@ -6,92 +6,51 @@
 /*   By: mbah <mbah@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/16 17:18:02 by mbah              #+#    #+#             */
-/*   Updated: 2025/01/16 17:52:34 by mbah             ###   ########.fr       */
+/*   Updated: 2025/01/18 01:01:07 by mbah             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "FdF_utils.h"
+#include "FdF.h"
 
-static void	my_mlx_draw_oblic1_line(t_data *img, t_point A, t_point B,
-	int color)
+static void	init_bresenham_vars(t_bresenham *vars, t_point p0, t_point p1)
 {
-	int	i;
-	int	j;
-
-	i = A.x;
-	j = 0;
-	while (i <= B.x)
-	{
-		my_mlx_pixel_put(img, A.x + j, A.y + j, color);
-		j++;
-		i++;
-	}
-}
-
-static void	my_mlx_draw_oblic2_line(t_data *img, t_point A, t_point B,
-	int color)
-{
-	int	i;
-	int	j;
-
-	i = B.x;
-	j = 0;
-	while (i >= A.x)
-	{
-		my_mlx_pixel_put(img, A.x + j, A.y - j, color);
-		j++;
-		i--;
-	}
-}
-
-static void	my_mlx_draw_samex_line(t_data *img, t_point A, t_point B, int color)
-{
-	int	inc;
-
-	if (A.y < B.y)
-		inc = 1;
+	vars->dx = abs(p1.x - p0.x);
+	vars->dy = abs(p1.y - p0.y);
+	if (p0.x < p1.x)
+		vars->sx = 1;
 	else
-		inc = -1;
+		vars->sx = -1;
+	if (p0.y < p1.y)
+		vars->sy = 1;
+	else
+		vars->sy = -1;
+	if (vars->dx > vars->dy)
+		vars->err = vars->dx / 2;
+	else
+		vars->err = -vars->dy / 2;
+}
+
+void	draw_line_bresenham(t_data *img, t_line line)
+{
+	t_bresenham	vars;
+	int			e2;
+
+	init_bresenham_vars(&vars, line.pt_src, line.pt_dst);
 	while (1)
 	{
-		my_mlx_pixel_put(img, A.x, A.y, color);
-		if (A.y == B.y)
+		my_mlx_pixel_put(img, line.pt_src.x, line.pt_src.y, line.color);
+		if (line.pt_src.x == line.pt_dst.x && line.pt_src.y == line.pt_dst.y)
 			break ;
-		if (inc == 1)
-			A.y += inc;
-		else
-			A.y += inc;
+		e2 = vars.err;
+		if (e2 > -vars.dx)
+		{
+			vars.err -= vars.dy;
+			line.pt_src.x += vars.sx;
+		}
+		if (e2 < vars.dy)
+		{
+			vars.err += vars.dx;
+			line.pt_src.y += vars.sy;
+		}
 	}
-}
-
-static void	my_mlx_draw_samey_line(t_data *img, t_point A, t_point B, int color)
-{
-	int	inc;
-
-	if (A.x < B.x)
-		inc = 1;
-	else
-		inc = -1;
-	while (1)
-	{
-		my_mlx_pixel_put(img, A.x, A.y, color);
-		if (A.x == B.x)
-			break ;
-		if (inc == 1)
-			A.x += inc;
-		else
-			A.x += inc;
-	}
-}
-
-void	my_mlx_draw_line(t_data *img, t_point A, t_point B, int color)
-{
-	if (A.y < B.y && A.x < B.x)
-		my_mlx_draw_oblic1_line(img, A, B, color);
-	else if (A.y > B.y && A.x < B.x)
-		my_mlx_draw_oblic2_line(img, A, B, color);
-	else if (A.x == B.x)
-		my_mlx_draw_samex_line(img, A, B, color);
-	else if (A.y == B.y)
-		my_mlx_draw_samey_line(img, A, B, color);
 }
--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: mbah <mbah@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/09 15:26:16 by mbah              #+#    #+#             */
-/*   Updated: 2025/01/17 18:34:41 by mbah             ###   ########.fr       */
+/*   Updated: 2025/01/18 00:52:17 by mbah             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,76 +30,22 @@ int	key_press(int keycode, t_vars *vars)
 	return (ERROR);
 }
 
-// Remplir l'image avec une couleur de fond (ici noir)
-void	clear_image(t_data *image, int width, int height)
-{
-	int	x;
-	int	y;
-
-	y = 0;
-	while (y < height)
-	{
-		x = 0;
-		while (x < width)
-		{
-			my_mlx_pixel_put(image, y, x, my_mlx_create_trgb(0, 0, 0, 0));
-			x++;
-		}
-		y++;
-	}
-}
-
-int	update_circle(t_vars *vars)
-{
-	mlx_clear_window(vars->mlx, vars->mlx_win);
-	clear_image(&vars->image, 520, 480);
-	
-	if (vars->keycode == KEY_UP)
-		vars->circle_pos.y -= 3;
-	if (vars->keycode == KEY_DOWN)
-		vars->circle_pos.y += 3;
-	if (vars->keycode == KEY_RIGHT)
-		vars->circle_pos.x += 3;
-	if (vars->keycode == KEY_LEFT)
-		vars->circle_pos.x -= 3;
-	vars->keycode = 0;
-	mlx_put_image_to_window(vars->mlx, vars->mlx_win, vars->moquette.img, 0, 0);
-	my_mlx_draw_circle(&vars->image, vars->circle_pos, 50,
-		my_mlx_create_trgb(0, 0, 0, 255));
-	//mlx_put_image_to_window(vars->mlx, vars->mlx_win, vars->image.img, 0, 0);
-	return (ERROR);
-}
-
 int	main(int argc, char **argv)
 {
-	t_vars		data;
-	t_point		pt;
+	t_vars	data;
+	t_line	red_line = {{100, 100}, {200, 200}, my_mlx_create_trgb(0, 255, 0, 0), 0};
 
 	if (argc < 2)
 		return (1);
-	pt.x = 100;
-	pt.y = 100;
-	data.circle_pos = pt;
 	data.mlx = mlx_init();
 	data.mlx_win = mlx_new_window(data.mlx, 520, 480, argv[1]);
 	data.image.img = mlx_new_image(data.mlx, 520, 480);
 	data.image.addr = mlx_get_data_addr(data.image.img,
 			&data.image.bits_per_pixel, &data.image.line_length,
 			&data.image.endian);
-	data.moquette.path = "./public/data/moq.xpm";
-	data.moquette.img = mlx_xpm_file_to_image(data.mlx, data.moquette.path,
-			&data.moquette.width, &data.moquette.height);
-	if (!data.moquette.img)
-	{
-		write(1, "Error\n", 6);
-		return (1);
-	}
 	mlx_hook(data.mlx_win, 17, 0, close_window, &data);
 	mlx_hook(data.mlx_win, 2, 1L << 0, key_press, &data);
-	mlx_put_image_to_window(data.mlx, data.mlx_win, data.moquette.img, 0, 0);
-	mlx_loop_hook(data.mlx, update_circle, &data);
-	my_mlx_draw_circle(&data.image, data.circle_pos, 50,
-		my_mlx_create_trgb(0, 0, 0, 255));
+	draw_line_bresenham(&data.image, red_line);
 	mlx_put_image_to_window(data.mlx, data.mlx_win, data.image.img, 0, 0);
 	mlx_loop(data.mlx);
 	return (0);
